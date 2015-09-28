bddc5cc454fba9dc05de640c9cd99041e2a5feca
seven332 <seven332@163.com>
2015-09-29 01:49:39 +0800

    Use SwitchCompat in FixedSwitchPreference

--- app/src/main/java/com/hippo/preference/FixedSwitchPreference.java
+++ app/src/main/java/com/hippo/preference/FixedSwitchPreference.java
@@ -18,9 +18,46 @@ package com.hippo.preference;
 
 import android.content.Context;
 import android.preference.SwitchPreference;
+import android.preference.TwoStatePreference;
+import android.support.v7.widget.SwitchCompat;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Checkable;
+import android.widget.CompoundButton;
+
+import com.hippo.nimingban.R;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class FixedSwitchPreference extends SwitchPreference {
+
+    private static Method sSyncSummaryViewMethod;
+
+    static {
+        try {
+            sSyncSummaryViewMethod = TwoStatePreference.class.getMethod("syncSummaryView", View.class);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            sSyncSummaryViewMethod = null;
+        }
+    }
+
+    private final Listener mListener = new Listener();
+
+    private class Listener implements CompoundButton.OnCheckedChangeListener {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if (!callChangeListener(isChecked)) {
+                // Listener didn't like it, change it back.
+                // CompoundButton will make sure we don't recurse.
+                buttonView.setChecked(!isChecked);
+                return;
+            }
+
+            FixedSwitchPreference.this.setChecked(isChecked);
+        }
+    }
 
     public FixedSwitchPreference(Context context) {
         super(context);
@@ -32,5 +69,37 @@ public class FixedSwitchPreference extends SwitchPreference {
 
     public FixedSwitchPreference(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onBindView(View view) {
+        super.onBindView(view);
+
+        View checkableView = view.findViewById(R.id.switchWidget);
+        if (checkableView != null && checkableView instanceof Checkable) {
+            if (checkableView instanceof SwitchCompat) {
+                final SwitchCompat switchView = (SwitchCompat) checkableView;
+                switchView.setOnCheckedChangeListener(null);
+            }
+
+            ((Checkable) checkableView).setChecked(isChecked());
+
+            if (checkableView instanceof SwitchCompat) {
+                final SwitchCompat switchView = (SwitchCompat) checkableView;
+                switchView.setTextOn(getSummaryOn());
+                switchView.setTextOff(getSummaryOff());
+                switchView.setOnCheckedChangeListener(mListener);
+            }
+        }
+
+        if (sSyncSummaryViewMethod != null) {
+            try {
+                sSyncSummaryViewMethod.invoke(this, view);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
