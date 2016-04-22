acfccaba6b62438aeb102c3319128c7f0988c363
seven332 <seven332@163.com>
2016-04-23 02:12:11 +0800

    Use https for AC HOST

--- app/src/main/java/com/hippo/nimingban/client/ac/ACUrl.java
+++ app/src/main/java/com/hippo/nimingban/client/ac/ACUrl.java
@@ -23,7 +23,7 @@ public class ACUrl {
 
     public static final String APP_ID = "nimingban";
 
-    public static final String HOST = "http://h.nimingban.com";
+    public static final String HOST = "https://h.nimingban.com";
 
     public static final String API_POST_LIST = HOST + "/Api/showf?appid=" + APP_ID;
 
