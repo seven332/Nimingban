87906ef42d06f8db4d2f47c2eefdc273b5a62558
seven332 <seven332@163.com>
2018-01-13 22:04:23 +0800

    Adjust the count of menu actions to show the whole adao url

--- app/src/main/java/com/hippo/nimingban/ui/ListActivity.java
+++ app/src/main/java/com/hippo/nimingban/ui/ListActivity.java
@@ -141,7 +141,6 @@ public final class ListActivity extends AbsActivity
 
     private MenuItem mRule;
     private MenuItem mCreatePost;
-    private MenuItem mRefresh;
     private MenuItem mSortForumsMenu;
 
     private @Nullable Forum mCurrentForum;
@@ -227,7 +226,6 @@ public final class ListActivity extends AbsActivity
                 if (mRightDrawer == view) {
                     setMenuItemVisible(mRule, true);
                     setMenuItemVisible(mCreatePost, true);
-                    setMenuItemVisible(mRefresh, true);
                     setMenuItemVisible(mSortForumsMenu, false);
                 }
             }
@@ -240,7 +238,6 @@ public final class ListActivity extends AbsActivity
                 if (mRightDrawer == view) {
                     setMenuItemVisible(mRule, false);
                     setMenuItemVisible(mCreatePost, false);
-                    setMenuItemVisible(mRefresh, false);
                     setMenuItemVisible(mSortForumsMenu, true);
                 }
             }
@@ -751,18 +748,15 @@ public final class ListActivity extends AbsActivity
         getMenuInflater().inflate(R.menu.activity_list, menu);
         mRule = menu.findItem(R.id.action_rule);
         mCreatePost = menu.findItem(R.id.action_create_post);
-        mRefresh = menu.findItem(R.id.action_refresh);
         mSortForumsMenu = menu.findItem(R.id.action_sort_forums);
 
         if (mDrawerLayout.isDrawerOpen(Gravity.RIGHT)) {
             mRule.setVisible(false);
             mCreatePost.setVisible(false);
-            mRefresh.setVisible(false);
             mSortForumsMenu.setVisible(true);
         } else {
             mRule.setVisible(true);
             mCreatePost.setVisible(true);
-            mRefresh.setVisible(true);
             mSortForumsMenu.setVisible(false);
         }
 
