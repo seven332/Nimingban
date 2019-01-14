9105d98f423dbae980244466868296ea6bbae6c3
WUGqnwvMQPzl <pkanna.on9@gmail.com>
2019-01-14 20:14:27 +0800

    Support jump page by manually type page number

--- app/src/main/java/com/hippo/nimingban/ui/fragment/PostFragment.java
+++ app/src/main/java/com/hippo/nimingban/ui/fragment/PostFragment.java
@@ -32,10 +32,13 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.InputFilter;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.text.style.ClickableSpan;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.StyleSpan;
@@ -46,6 +49,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -65,6 +69,7 @@ import com.hippo.nimingban.client.data.Reply;
 import com.hippo.nimingban.client.data.Site;
 import com.hippo.nimingban.ui.GalleryActivity2;
 import com.hippo.nimingban.ui.PostActivity;
+import com.hippo.nimingban.util.MinMaxFilter;
 import com.hippo.nimingban.util.OpenUrlHelper;
 import com.hippo.nimingban.util.ReadableTime;
 import com.hippo.nimingban.util.Settings;
@@ -403,12 +408,13 @@ public class PostFragment extends BaseFragment
     }
 
     private class GoToDialogHelper implements View.OnClickListener,
-            DialogInterface.OnDismissListener {
+            DialogInterface.OnDismissListener, Slider.OnSetProgressListener, TextWatcher {
 
         private int mPages;
 
         private View mView;
         private Slider mSlider;
+        private EditText mEditText;
 
         private Dialog mDialog;
 
@@ -421,6 +427,12 @@ public class PostFragment extends BaseFragment
             mSlider = (Slider) mView.findViewById(R.id.slider);
             mSlider.setRange(1, pages);
             mSlider.setProgress(currentPage + 1);
+            mSlider.setOnSetProgressListener(this);
+            mEditText = (EditText) mView.findViewById(R.id.page_input);
+            mEditText.setText(Integer.toString(currentPage + 1)); // Android still treating int as resid
+            mEditText.setHint("1");
+            mEditText.setFilters(new InputFilter[] {new MinMaxFilter(1, pages)});
+            mEditText.addTextChangedListener(this);
         }
 
         public View getView() {
@@ -451,6 +463,30 @@ public class PostFragment extends BaseFragment
         public void onDismiss(DialogInterface dialog) {
             mDialog = null;
         }
+
+        @Override
+        public void onSetProgress(Slider slider, int newProgress, int oldProgress, boolean byUser, boolean confirm) {
+            if (!byUser) return;
+            mEditText.setText(String.valueOf(newProgress)); // To prevent Android treat progress as resid
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            int i = s.length() > 0 ? Integer.parseInt(s.toString()) : 1;
+            mSlider.setProgress(i);
+        }
+
+        @Override
+        public void onFingerDown() {}
+
+        @Override
+        public void onFingerUp() {}
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+        @Override
+        public void afterTextChanged(Editable s) {}
     }
 
 
